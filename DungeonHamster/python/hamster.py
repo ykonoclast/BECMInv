c44e1d342cdcf890f97b5d22b3cd0b0dfabc6a97
Ykon O'Clast <ykonoclast@protonmail.com>
2023-04-07 15:55:28 +0200

    recap total du bas fonctionnel

--- DungeonHamster/python/hamster.py
+++ DungeonHamster/python/hamster.py
@@ -1,4 +1,5 @@
 from browser import document, console, window, html
+from bisect import bisect_right
 
 #TODO voir si on met tous les binds à la fin d'un seul coup ou si on les laisse répartis.
 
@@ -34,6 +35,58 @@ def get_row_info(cellule):
 	return tbody, nbrows, row_index, row
 
 #SECTION gestion des valeurs numériques
+
+#TODO voir pour extraire cela en json si possible, car pure data de config
+list_mvt=[{"turn":120,"round":40},{"turn":90,"round":30},{"turn":60,"round":20},{"turn":30,"round":10},{"turn":15,"round":5},{"turn":0,"round":0}]
+list_enc_thresh=[0,401,801,1201,1601,2401]
+
+def get_speeds(enc):
+	index=bisect_right(list_enc_thresh,enc)-1#TODO commenter plus avant cette ligne...
+	return list_mvt[index]
+
+def get_section_enc(id):
+	#TODO execption si id existe pas
+	recap = document[id]
+	sec_tot = recap.getElementsByClassName("Sec_Tot")[0]
+	return int(sec_tot.text)
+
+def update_main_recap():
+	sacdos=get_section_enc("recap_sacdos_id")
+	grossac=get_section_enc("recap_grossac_id")
+	bourse=get_section_enc("recap_bourse_id")
+	petitsac1=get_section_enc("recap_petitsac1_id")
+	petitsac2=get_section_enc("recap_petitsac2_id")
+	petitsac3=get_section_enc("recap_petitsac3_id")
+	porte=get_section_enc("recap_porte_id")
+
+	total = sacdos + grossac + bourse + petitsac1 + petitsac2 + petitsac3 + porte
+	sanssacdos = total - sacdos
+	sanssacsclass = total - petitsac1 - petitsac2 - petitsac3 - grossac
+	justebourseporte = porte + bourse
+
+	document["total_id"].text=total
+	document["sanssacdos_id"].text=sanssacdos
+	document["sanssacsclass_id"].text=sanssacsclass
+	document["justebourseporte_id"].text=justebourseporte
+
+	total_speed_byturn, total_speed_byround = get_speeds(total).values()
+	sanssacdos_speed_byturn, sanssacdos_speed_byround = get_speeds(sanssacdos).values()
+	sanssacsclass_speed_byturn, sanssacsclass_speed_byround = get_speeds(sanssacsclass).values()
+	justebourseporte_speed_byturn, justebourseporte_speed_byround = get_speeds(justebourseporte).values()
+
+	document["total_byround_id"].text=total_speed_byround
+	document["sanssacdos_byround_id"].text=sanssacdos_speed_byround
+	document["sanssacsclass_byround_id"].text=sanssacsclass_speed_byround
+	document["justebourseporte_byround_id"].text=justebourseporte_speed_byround
+
+	document["total_byturn_id"].text=total_speed_byturn
+	document["sanssacdos_byturn_id"].text=sanssacdos_speed_byturn
+	document["sanssacsclass_byturn_id"].text=sanssacsclass_speed_byturn
+	document["justebourseporte_byturn_id"].text=justebourseporte_speed_byturn
+
+
+
+
 def update_enc(section):
 	#calcul du contenu de l'emplacement
 	sec_enc=section.getElementsByClassName("Sec_Enc")[0]
@@ -62,8 +115,11 @@ def update_enc(section):
 			maxi=int(sec_max.text)
 			if items_total>maxi:
 				color="red"
-
 	sec_enc.style.color=sec_tot.style.color=color
+
+	#mise à jour du récap total en bas
+	update_main_recap()
+
 
 #SECTION creation et suppression des rows des tables
 def make_new_row(tbody):
