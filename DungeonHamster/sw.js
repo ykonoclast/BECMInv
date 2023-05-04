4f97ba7c1ecbbe0f84d044ebee05d0c9f0b068a1
Ykon O'Clast <ykonoclast@protonmail.com>
2023-05-04 22:24:14 +0200

    gros nettoyage du code du service worker passage du code de persistance en python modification des tailles de police pour téléphone

--- DungeonHamster/sw.js
+++ DungeonHamster/sw.js
@@ -1,16 +1,26 @@
 
-const CACHE_NAME = "V2";//TODO changer ces noms pas terribles
-const STATIC_CACHE_URLS = ["/", "favicon.ico", "css/styles.css", "python/main.bry", "js/brython.js", "js/brython_stdlib.js", "icons/logo.png", "icons/logo192.png", "icons/logo512.png", "fonts/EBGaramond-Italic.otf", "fonts/EBGaramond-Regular.otf", "fonts/EBGaramond-SemiBold.otf"];
-//TODO COMMENTER PARTOUT!!!!!
-//PREFIXER CHAQUE LOG PAR LE PREFIX "cache_name" (a changer)
+const CACHE_VERSION = "vALPHA14";//TODO aussi changer la version du cache à terme
+//Noter ci-dessous la présence de /, pas certain que ça marche avec index.html
+const LIST_PRE_CACHE = ["/", "favicon.ico", "manifest.json", "css/styles.css", "python/main.bry", "js/brython.js", "js/brython_stdlib.js", "icons/logo.png", "icons/logo192.png", "icons/logo512.png", "fonts/EBGaramond-Italic.otf", "fonts/EBGaramond-Regular.otf", "fonts/EBGaramond-SemiBold.otf"];
+//TODO ajouter manifest.json au pre-cache, vérifier que ça marche encore et commenter le fait que ça y soit
 
-async function pre_cache()
+
+
+//TODO check enregistrement service worker propre
+
+
+//Noter que l'on pourrait se passer du pré-cache dans la mesure où l'appli est simple : tout est donc fetché rapidement dès le début (et donc serait mis en cache ainsi). Mais pour une appli plus compliquée il faut pré-cacher les assets qui ne sont pas forcément fetchés dès le début si l'on veut plus tard y accéder hors ligne avant qu'un fetch réseau ne soit arrivé
+//cette fonction est async surtout pour renvoyer une promesse sur laquelle pourra attendre le event.waituntil de when_install : sinon dans l'absolu il n'y aurait rien de choquant à ce qu'elle soit, justement, synchrone, pour finir de bien charger le cache avant d'aller plus loin.
+async function pre_cache()//la fonction async attendra sur des await pendant que le thread principal continue, checkant le await au prochain tick
 {
-	try {
-		console.log("Opening cache...");//todo indiquer version, de manière générale voir pour logging de bonne qualité
-		const lecache = await caches.open(CACHE_NAME);
-		lecache.addAll(STATIC_CACHE_URLS);
-	} catch (e)
+	try
+	{
+		console.log(`Pre-caching for cache ${CACHE_VERSION}...`);
+		//le couple "const" et "await" remplace ce qui se trouve respectivement après et avant le "then" en syntaxe de promise (en ce cas : caches.open(CACHE_VERSION).then(cache=>cache.addALL(LIST_PRE_CACHE))
+		const currentCache = await caches.open(CACHE_VERSION);//pas d'attente active sur l'ouverture du cache
+		currentCache.addAll(LIST_PRE_CACHE);
+	}
+	catch (e)
 	{
 		console.error(e);
 	}
@@ -18,11 +28,13 @@ async function pre_cache()
 
 function when_install(event)
 {
-	self.skipWaiting();
+	self.skipWaiting();//on n'attend pas la fin de vie du précédent service worker : on le supprime pour installer le nouveau directement.
 	console.log("Service Worker installing...");
-	try {
-		event.waitUntil(pre_cache());
-	} catch (e)
+	try
+	{
+		event.waitUntil(pre_cache());//le service worker ne passera au stade d'ACTIVATION que APRÈS tous les traitements de pré-cache
+	}
+	catch (e)
 	{
 		console.error(e);
 	}
@@ -31,43 +43,183 @@ function when_install(event)
 self.addEventListener("install", when_install);
 
 
-self.addEventListener("activate", event => {
+//NOTE : cette fonction était auparavant async et le caches.delete était en await mais cela ne semblait pas nécessaire : pas de race condition sur la suppression d'une clé et pas de waituntil dans le code appelant voulant une promesse à tout prix
+function delete_single_cache(key)
+{
+	try
+	{
+		console.log(`Deleting older cache ${key}...`);
+		caches.delete(key);
+	}
+	catch (e)
+	{
+		console.error(e);
+	}
+}
+
+//cette fonction est async surtout pour renvoyer une promesse sur laquelle pourra attendre le event.waituntil de when_activate : sinon dans l'absolu il n'y aurait rien de choquant à ce qu'elle soit, justement, synchrone, pour finir de bien nettoyer le cache avant d'aller plus loin.
+async function delete_older_caches()
+{
+	try
+	{
+		const cacheKeys = await caches.keys();
+		keysToDelete = cacheKeys.filter(key => key !== CACHE_VERSION);
+		await Promise.all(keysToDelete.map(delete_single_cache));//le promise.all groupe toutes les promesses du tableau, donc on attend la suppression de toutes les clés pour sortir de la méthode ; la fonction n'est pas async MAIS promise.all wrappera le résultat dans une promesse donc ça va.
+	}
+	catch (e)
+	{
+		console.error(e);
+	}
+}
+
+
+
+function when_activate(event)
+{
 	console.log("Service Worker activating...");
-	clients.claim();//permet de contrôler tout de suite la page sans attendre un rechargement (n'interceptera pas les fetch avant un deuxième chargement sinon par exemple)
+	clients.claim(); //permet de contrôler tout de suite la page sans attendre un rechargement (n'interceptera pas les fetch avant un deuxième chargement sinon par exemple)
+	try
+	{
+		event.waitUntil(delete_older_caches());
+	}
+	catch (e)
+	{
+		console.error(e);
+	}
+}
 
-	// delete any unexpected caches
-	event.waitUntil(
-			caches
-			.keys()
-			.then(keys => keys.filter(key => key !== CACHE_NAME))
-			.then(keys =>
-				Promise.all(
-						keys.map(key => {
-							console.log(`Deleting cache ${key}`);
-							return caches.delete(key);
-						})
-						)
-			)
-			);
-});
 
+self.addEventListener("activate", when_activate);
+
+//POUR ARCHIVE : le code ci-dessous est l'ancien fonctionnement de l'activation, avant changement de syntaxe de then à async
 /*
- self.addEventListener("fetch", event => {
- console.log(`event request url : ${event.request.url}`);
- event.respondWith(
- fetch(event.request)
- .catch(error => {
- console.log("FROM CACHE");
- return caches.match(event.request);
+ self.addEventListener("activate", event => {
+ console.log("Service Worker activating...");
+ clients.claim();//permet de contrôler tout de suite la page sans attendre un rechargement (n'interceptera pas les fetch avant un deuxième chargement sinon par exemple)
+
+ // delete any unexpected caches
+ event.waitUntil(
+ caches
+ .keys()
+ .then(keys => keys.filter(key => key !== CACHE_VERSION))
+ .then(keys =>
+ Promise.all(
+ keys.map(key => {
+ console.log(`Deleting cache ${key}`);
+ return caches.delete(key);
  })
+ )
+ )
  );
  });
  */
 
+
+async function cache_request(request, response)
+{
+	if (response.type !== "error" && response.type !== "opaque")
+	{//on ne cache pas les erreurs réseau
+		try
+		{
+			console.log(`caching url:${request.url}`);
+			const currentCache = await caches.open(CACHE_VERSION);
+			currentCache.put(request, response.clone());
+		}
+		catch (e)
+		{
+			console.error(e);
+		}
+	}
+}
+
+async function fetch_response(req)
+{
+	try
+	{
+		let response = await caches.match(req);//on attend d'avoir la réponse pour progresser, sans attente active
+
+		if (!response)
+		{//la réponse est vide : le cache ne contient pas la valeur recherchée : on va donc appeler le réseau
+			console.log(`url:${req.url} not in cache : fetching through network`);
+			response = await fetch(req);//on fetch, sans attente active
+			await cache_request(req, response);//CRUCIAL : sans cela on passe à la suite et la réponse est traitée AVANT la mise en cache (car cache_request est async! Le flot continue donc pendant ses propres await) : qui ne peut donc se faire car la réponse a déjà été consommée
+		}
+		return response;
+	}
+	catch (e)
+	{
+		console.error(e);
+	}
+}
+
+function when_fetch(event)
+{
+	{
+		let req = event.request;
+		console.log(`fetching url:${req.url}`);
+		if (req.url.includes("main.bry"))
+		{//cas particulier de brython qui appelle ses scripts en les postfixant d'une numéro, on le strippe avant de fetcher pour obtenir le vrai nom de fichier
+			console.log("stripping brython url");
+			const newUrl = event.request.url.split('?')[0];
+			req = new Request(newUrl, event.request);
+		}
+
+		// Cache-First Strategy : si l'appli est mise à jour, il faudra mettre à jour la version du service worker pour qu'il re-précache. Comme l'appli n'a pas BESOIN d'assets réseau (pas de données) c'est plus efficient car ça évite des fetch réseau inutiles
+		//à noter que pour une appli échangeant effectivement des données il vaudrait mieux une stratégie network first VOIRE différencier entre pages et objets json, de façon hybride
+		response = fetch_response(req);
+		event.respondWith(response);
+	}
+}
+
+self.addEventListener("fetch", when_fetch);
+
+
+//POUR ARCHIVE : ancien code de fetch, en syntaxe then et pas async
+/*self.addEventListener("fetch", event => {
+ console.log(`FETCH URL : ${event.request.url}`);
+ let req = event.request;
+ if (event.request.url.includes("main.bry"))
+ {
+ console.log("bry détecté");
+ const newUrl = event.request.url.split('?')[0];
+ console.log(newUrl);
+
+ req = new Request(newUrl, event.request);
+ }
+ console.log(`NEW URL : ${req.url}`);
+ // Cache-First Strategy
+ //TODO expliquer pourquoi finalement on part en cache-first
+ event.respondWith(
+ caches
+ .match(req) // check if the request has already been cached
+ .then(cached => cached || fetch(req)) // otherwise request network
+ .then(
+ response =>
+ cache(req, response) // put response in cache
+ .then(() => response) // resolve promise with the network response
+ )
+ );
+ });
+
+ function cache(request, response) {
+ if (response.type === "error" || response.type === "opaque") {
+ return Promise.resolve(); // do not put in cache network errors
+ }
+
+ return caches
+ .open(CACHE_VERSION)
+ .then(cache => cache.put(request, response.clone()));
+ }
+ */
+
+
+
+
+//POUR ARCHIVE : ci-dessous un brouillon de stratégie network-first, ne marchant pas très bien (du fait du problème du 'NAVIGATE' ; pourrait être réglé en différenciant plutôt le format des url (entre une page et un JSON pour une stratégie de cache hybride)
 /*self.addEventListener("fetch", event => {
  console.log(`event request : ${event.request}`);
 
- if (event.request.mode === 'navigate')//il s'agit d'une requêtre liée à la navigation de l'utilisateur sur le domaine du site
+ if (event.request.mode === 'navigate')//il s'agit d'une requêtre liée à la navigation de l'utilisateur sur le domaine du site : ATTENTION, dans les tests ça ne marchait PAS car le browser n'utilisait pas navigate pour les pages
  {
  console.log("NAVIGATE");
  event.respondWith((async() => {
@@ -84,8 +236,8 @@ self.addEventListener("activate", event => {
  } catch (e)//pas moyen de récupérer une réponse
  {
  console.log("CACHE");
- const lecache = await caches.open(CACHE_NAME);//TODO factoriser
- return await lecache.match(event.request);
+ const currentCache = await caches.open(CACHE_VERSION);//TODO factoriser
+ return await currentCache.match(event.request);
  }
  })());
  }
@@ -93,43 +245,3 @@ self.addEventListener("activate", event => {
 
  });
  */
-
-
-
-
-//TODO commenter l'intégralité de cette merde et refactorer (mais pas dans ce sens là)
-self.addEventListener("fetch", event => {
-	console.log(`FETCH URL : ${event.request.url}`);
-	let req = event.request;
-	if (event.request.url.includes("main.bry"))
-	{
-		console.log("bry détecté");
-		const newUrl = event.request.url.split('?')[0];
-		console.log(newUrl);
-
-		req = new Request(newUrl, event.request);
-	}
-	console.log(`NEW URL : ${req.url}`);
-	// Cache-First Strategy
-	event.respondWith(
-			caches
-			.match(req) // check if the request has already been cached
-			.then(cached => cached || fetch(req)) // otherwise request network
-			.then(
-					response =>
-				cache(req, response) // put response in cache
-						.then(() => response) // resolve promise with the network response
-			)
-			);
-});
-
-
-function cache(request, response) {
-	if (response.type === "error" || response.type === "opaque") {
-		return Promise.resolve(); // do not put in cache network errors
-	}
-
-	return caches
-			.open(CACHE_NAME)
-			.then(cache => cache.put(request, response.clone()));
-}
